a75c3473eccdd8cf1e984a7ea79c72aa1eeb144a
K0opel <rafiwitaqi@gmail.com>
2023-07-23 23:12:16 +0700

    uppp

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,11 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('b1ywi8yntr2twxgtz1ac', 'uyyadb4imvfv0ran', 'W6a4OUseGKxeF69WqdLG', {
     host: 'b1ywi8yntr2twxgtz1ac-mysql.services.clever-cloud.com',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    dialectOptions: {
+        connectTimeout: 60000, // Set a longer connection timeout (in milliseconds)
+      },
+      timezone: '+07:00'
 });
 
 export default db;
