f3ffca0f1aa307c00e9119e72caed23b5068e7b2
K0opel <rafiwitaqi@gmail.com>
2023-07-23 22:51:59 +0700

    mid

---
+++ config/Database.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize('b1ywi8yntr2twxgtz1ac', 'uyyadb4imvfv0ran', 'W6a4OUseGKxeF69WqdLG', {
+    host: 'b1ywi8yntr2twxgtz1ac-mysql.services.clever-cloud.com',
+    dialect: 'mysql',
+    port: 3306
+});
+
+export default db;
